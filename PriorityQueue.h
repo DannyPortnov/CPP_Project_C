b3758592aac01af6641de1157ca99c2b6d5602e0
danih <danih@DESKTOP-GCQI5FM>
2023-01-19 18:25:09 +0200

    Start of PriorityQueue

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -19,32 +19,84 @@ using namespace std;
 #endif
 #endif  // _DEBUG
 
+template <class T>
 class PriorityQueue {
 private:
-	static mt19937 m_gen;    // Random-number engine used (Mersenne-Twister in this case)
-	char* m_queue;	//queue array pointer
-	char* m_head;
-	char* m_tail;
-	const int m_service_time;
-	const int m_capacity;	// the queue's capacity 
-	unsigned m_variables_count = 0; // number of variables in queue
-	static unsigned queue_number; // number of class instances
-	PriorityQueue& initialize_queue();
-	void increment_pointer(char*& pointer); //ref to pointer
+	T* m_queue;	//queue array pointer
+	T* m_head;
+	T* m_tail;
+	const unsigned m_priority;
+	unsigned m_size = 0; // number of variables in queue
+	void increment_pointer(T*& pointer) {
+		if (pointer == &m_queue[m_capacity - 1]) {
+			pointer = &m_queue[0];
+		}
+		else {
+			pointer++;
+		}
+	}
 public:
-	PriorityQueue(int capacity);
-	PriorityQueue(const PriorityQueue&);
-	~PriorityQueue();		// deconstructor
-	void push(const char new_client);
-	const bool pop();
-	const bool is_queue_full() const;
-	const bool is_queue_empty() const;
-	const char front() const;
-	const int size() const;
-	const int get_service_time() const;
-	static mt19937& get_random_num_gen();
+	PriorityQueue::PriorityQueue(int capacity)
+		:m_capacity(capacity), m_head(nullptr)
+	{
+		initialize_queue();
+	}
+	PriorityQueue(const PriorityQueue&) {
+		m_queue = new char[m_capacity];
+		m_tail = &m_queue[0];
+		return *this;
+	}
+	~PriorityQueue() {// deconstructor
+		delete[] m_queue;
+	}		
+	void push(const T new_client) {
+		if (!is_queue_full()) {
+			*m_tail = new_client;
+			increment_pointer(m_tail);
+			m_variables_count++;
+		}
+		else {
+			return;
+		}
+		if (is_queue_empty()) { //checks if m_head is nullptr, not really empty because a client was pushed.
+			if (m_tail == &m_queue[0]) {
+				m_head = &m_queue[m_capacity - 1];
+			}
+			else {
+				m_head = (m_tail - 1);
+			}
+		}
+	}
+	const bool pop() {
+		if (is_queue_empty()) {
+			return false;
+		}
+		*m_head = NULL; //remove client
+		increment_pointer(m_head);
+		if (m_tail == m_head) {
+			m_head = nullptr;
+		}
+		m_size--;
+
+		return true;
+	}
+	const bool is_queue_full()  const {
+		return m_tail == m_head;
+	}
+
+	const bool is_queue_empty()  const {
+		return m_head == nullptr;
+	}
+	const char front() const {
+		if (!is_queue_empty())
+			return *m_head;
+		cout << "The Queue Is Empty!" << endl;
+	}
+
+	const int size() const {
+		return m_size;
+	}
 };
 
-const int random_func(const int min, const int max);
 
 #endif		// _QUEUE_H_
