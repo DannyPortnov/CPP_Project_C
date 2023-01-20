ce2234b9d8c76b304455ec2bed6212966c663eb7
danih <danih@DESKTOP-GCQI5FM>
2023-01-20 09:59:22 +0200

    Before writing ChatGPT's version

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -25,7 +25,8 @@ class PriorityQueue {
 		PQ_Item<T>** m_queue;	//queue array pointer
 		PQ_Item<T>* m_head;
 		PQ_Item<T>* m_tail;
-		int m_size = 0; // number of variables in queue
+		int m_size = 0; // current number of variables in queue
+
 		void increment_pointer(PQ_Item<T>*& pointer) {
 
 			if (pointer == m_queue[m_size - 1]) {
@@ -42,24 +43,27 @@ class PriorityQueue {
 				int i = 0;
 				for (; i < m_size; i++)
 				{
-					PQ_Item<T>* test = m_queue[i]; //compilter doesn't handle autocompletes appropriately!
-					if (test->get_priority ) {
+					PQ_Item<T>* queue_element = m_queue[i]; //compilter doesn't handle autocompletes appropriately!
+					if (queue_element->get_priority() < item_to_push.get_priority()) {
+						break;
 					}
+
 				}
+
 				*m_tail = new_item;
 				increment_pointer(m_tail);
 			}
 			else {
 				return;
 			}
-			//if (is_queue_empty()) { //checks if m_head is nullptr, not really empty because a client was pushed.
-			//	if (m_tail == &m_queue[0]) {
-			//		m_head = &m_queue[m_size - 1];
-			//	}
-			//	else {
-			//		m_head = (m_tail - 1);
-			//	}
-			//}
+			if (is_queue_empty()) { //checks if m_head is nullptr, not really empty because a client was pushed.
+				if (m_tail == &m_queue[0]) {
+					m_head = &m_queue[m_size - 1];
+				}
+				else {
+					m_head = (m_tail - 1);
+				}
+			}
 		}
 
 		PriorityQueue(int size)
